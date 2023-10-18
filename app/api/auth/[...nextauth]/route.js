d4e024bc98df03abc9cb39ac70f7b49cde41f756
Rachel Spencer <rachelspencer688@gmail.com>
2023-10-18 12:36:47 -0400

    connect mongodb

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+import { connectToDB } from "@utils/database";
+
+
 const handler = NextAuth({
     providers: [
         GoogleProvider({
@@ -12,7 +15,18 @@ const handler = NextAuth({
 
     },
     async signIn({ profile }) {
+        try {
+            await connectToDB();
 
+            // check if a user already exists
+
+            // if not create a new user
+
+            return true;
+        } catch (error) {
+            console.log(error)
+            return false;
+        }
     }
 });
 
