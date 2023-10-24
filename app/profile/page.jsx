a7a03737ee9642ff0358a4a19cd0ec5b72793144
Rachel Spencer <rachelspencer688@gmail.com>
2023-10-24 14:12:03 -0400

    complete handleDelete function

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -29,7 +29,22 @@ const MyProfile = () => {
 
     };
 
-    const handleDelete = async () => {
+    const handleDelete = async (post) => {
+      const hasConfirmed = confirm("Are you sure you want to delete this prompt?")
+
+      if (hasConfirmed) {
+        try {
+          await fetch(`/api/prompt/${post._id.toString()}`, {
+            method: "DELETE"
+          })
+          // repost all posts excluding the one just deleted
+
+          const filteredPosts = posts.filter((p) => p._id !== post._id)
+          setPosts(filteredPosts)
+        } catch (error) {
+          console.log(error)
+        }
+      }
 
     };
 
