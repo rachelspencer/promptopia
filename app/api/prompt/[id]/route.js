061351d0f5273c2e88eb99ceea97d42f69247de8
Rachel Spencer <rachelspencer688@gmail.com>
2023-10-24 12:30:06 -0400

    create DELETE api endpoint to handle prompt deletion dynamically with user id

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -37,5 +37,17 @@ export const PATCH = async (request, { params }) => {
     }
 }
 
-
 // DELETE (delete)
+export const DELETE = async (request, { params }) => {
+    try {
+
+        await connectToDB();
+
+        await Prompt.findByIdAndRemove(params.id);
+        
+        return new Response("Prompt deleted successfully", { status: 200 });
+
+    } catch (error) {
+        return new Response("Failed to delete prompt", { status: 500 });
+    }
+}
