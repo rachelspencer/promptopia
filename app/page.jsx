eefde88ebd9d9ec1a8acec16c4b9781082578d38
Rachel Spencer <rachelspencer688@gmail.com>
2023-10-27 18:52:58 -0400

    update home page tag line

--- app/page.jsx
+++ app/page.jsx
@@ -9,7 +9,7 @@ const Home = () => {
             <span className="orange_gradient text-center">AI-Powered Prompts</span>
         </h1>
         <p className="desc text-center">
-        Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts
+        Promptopia is an open-source AI prompting tool for the modern world to create, discover and share prompts
         </p>
         <Feed/>
     </section>
