5105dacf0ac665f32ad231c635917ff40336d3af
Rachel Spencer <rachelspencer688@gmail.com>
2023-11-06 15:43:37 -0500

    remove empty string from signIn function

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -63,7 +63,7 @@ const Nav = () => {
               <button
               type="button"
               key={provider.name}
-              onClick={() => signIn(provider.id)}
+              onClick={() => signIn()}
               className="black_btn "
               >
                 Sign In
